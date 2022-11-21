60481faf2f8e208903896042ad824b23aa23a579
Ludwig Ruderstaller <l.ruderstaller@cwd.at>
2022-11-21 22:16:10 +0100

    bugfixes

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,5 @@
-install_hooks()
-{
-    report_status "Installing git hooks"
-        if [[ ! -e /home/pi/klipper/.git/hooks/post-merge ]]
-        then
-           ln -s /home/pi/klipper_stuff/scripts/post-merge.sh /home/pi/klipper/.git/hooks/post-merge
-        fi
-}
-
-install_hooks()
+#!/bin/bash
+if [[ ! -e /home/pi/klipper/.git/hooks/post-merge ]]
+then
+    ln -s /home/pi/klipper_stuff/scripts/post-merge.sh /home/pi/klipper/.git/hooks/post-merge
+fi
