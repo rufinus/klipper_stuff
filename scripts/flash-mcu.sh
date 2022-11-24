51a124614ea1746a7eb2c282491c25edd6917a98
Ludwig Ruderstaller <l.ruderstaller@cwd.at>
2022-11-24 16:28:35 +0100

    fixing update

--- scripts/flash-mcu.sh
+++ scripts/flash-mcu.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+# NOTE: This script ONLY WORKS if there's no SD card in the board AND the
+# board has already been flashed with klipper via SD card.
+# NEXT SECTION IS ONLY RELEVANT IF USING THE boot0 JUMPER (NOT RECOMMENDED)
+# The first time the board is booted after the jumper has been installed
+# the board needs to be flashed via the dfu vendor:device id. After that
+# it can be flashed via the /dev/btt-octopus-11 path, but it then fails
+# to exit dfu mode, and then one needs to flash it yet again with the
+# vendor:device id. Then it correctly exits DFU mode. Except those times
+# where it doesn't, for that we have a 3rd pass...
+# Mikl Schmidt - source: https://github.com/Rat-OS/RatOS-configuration/blob/master/boards/btt-octopus-11/make-and-flash-mcu.sh
+
 if [ "$EUID" -ne 0 ]
   then echo "ERROR: Please run as root"
   exit
 fi
 
+MCU=/dev/btt-octopus-11
+VENDORDEVICEID=0483:df11
 
 cp -f /home/pi/klipper_stuff/config/board/btt-octopus-11/firmware.config /home/pi/klipper/.config
 pushd /home/pi/klipper
@@ -12,18 +25,37 @@ make clean
 make -j4
 
 service klipper stop
-make flash FLASH_DEVICE=/dev/btt-octopus-11
-
+if [ -h $MCU ]; then
+    echo "Flashing Octopus via path"
+    make flash FLASH_DEVICE=$MCU
+else
+    echo "Flashing Octopus via vendor and device ids - 1st pass"
+    make flash FLASH_DEVICE=$VENDORDEVICEID
+fi
+sleep 5
 if [ -h $MCU ]; then
     echo "Flashing Successful!"
 else
-    echo "Flashing failed :("
-    service klipper start
-    popd
-    chown pi:pi -R /home/pi/klipper
-    exit 1
-fi
+    echo "Flashing Octopus via vendor and device ids - 2nd pass"
+    make flash FLASH_DEVICE=$VENDORDEVICEID
 
+    sleep 5
+    if [ -h $MCU ]; then
+        echo "Flashing Successful!"
+    else
+        echo "Flashing Octopus via vendor and device ids - 3rd pass"
+        make flash FLASH_DEVICE=$VENDORDEVICEID
+        if [ $? -e 0 ]; then
+            echo "Flashing successful!"
+        else
+            echo "Flashing failed :("
+            service klipper start
+            popd
+            chown pi:pi -R /home/pi/klipper
+            exit 1
+        fi
+    fi
+fi
 chown pi:pi -R /home/pi/klipper
 service klipper start
 popd
